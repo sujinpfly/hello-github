3bc9b729fede5c750de7a8dc5ea28c2eea4b1f0e
sujinpfly <sujinp@126.com>
2016-11-23 22:05:23 +0800

    tesst
    test hh

---
+++ test.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print ('hello world')
+
+
+# In[6]:
+
+import turtle
+turtle.fd(100)
+turtle.right(144)
+turtle.fd(100)
+
+
+# In[9]:
+
+s='hello,world.'
+str(s)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
